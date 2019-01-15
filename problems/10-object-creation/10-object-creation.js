b2dd62180942216ad13e3bbffb9e661eb67f197f
Kyle Serrecchia <kyserrecchia@gmail.com>
2019-01-15 15:31:21 -0500

    object creation done

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,29 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+let SpiderMan = {
+    name: 'Peter Parker',
+    age: 16
+}
+
+function SpiderHam(name, age){
+    this.name = 'Peter Parker';
+    this.age = 16;
+}
+
+let SpiderHammy = new SpiderHam('Peter Porker', 40);
+
+class ModernSpider {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let MilesMorales = new ModernSpider('Miles Morales', 14);
+
+
+
+
+
