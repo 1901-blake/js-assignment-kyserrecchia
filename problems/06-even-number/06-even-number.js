97962df38f135437896d8725bcc1ecce5a60823c
Kyle Serrecchia <kyserrecchia@gmail.com>
2019-01-15 11:06:17 -0500

    even number done

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -2,6 +2,12 @@
 Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
-function isEven(someNum) {
-  
+
+function isEven(k) {
+    return Math.round(k/2)==k/2 ? true : false;
+}
+
+//testing numbers up to 10
+for(let i=0;i<10;i++){
+    console.log(`${i}: ${isEven(i)}`);
 }
