a8a092d2f740ffce8b44e1df8746409e591d5530
Kyle Serrecchia <kyserrecchia@gmail.com>
2019-01-15 11:19:34 -0500

    palindrome done

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,19 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
+
+
 function isPalindrome(someStr) {
-  
+    let len = someStr.length;
+    let halfLen = len/2;
+    let i=0;
+    while(i<halfLen){
+        if(someStr[len-i-1]!=someStr[i]){
+            return false;
+        }
+        i++;
+    }
+    return true;
 }
+
+console.log(isPalindrome("cac"));
